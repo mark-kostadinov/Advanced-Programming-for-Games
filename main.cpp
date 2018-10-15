bfa717e940a1e0a5c4d509bcc8437012d8d5016d
Mark Kostadinov <b5036861@newcastle.ac.uk>
2018-10-15 11:46:04 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+/// >> Number class (may be culled?): to encapsulate a lock number (should be 0 to 9), as well as
+/// functions for turning a number (e.g. from 1 to 7 is either 2->6 or 0->8),
+/// 
+/// >> Lock class: (Number, Number, Number, Number), bool isPressed, function for pressing a button and one for unlocking
+/// - LockTheLock() -> generate a ROOT integer and a CN as a result of hashing the ROOT
+/// 
+/// 
+///
+///
+///
+///
+///
+///
+///
+///
+///
+/// >> Safe class: constructor(int numOfLocks) -> add a new Lock each time to the class variable vector of Locks, one Lock by default
+/// Start with a simple constructor for one lock, then 5 locks, then the above solution.
+
+int main()
+{
+	return 0;
+}
